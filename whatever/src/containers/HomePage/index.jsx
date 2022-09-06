7b076068b96cec7c6768ef4286a6581a5aa1c3f0
Alptekin <67191456+AlptekinOcakdan@users.noreply.github.com>
2022-09-06 14:37:42 +0300

    A Shitshow.

---
+++ whatever/src/containers/HomePage/index.jsx
@@ -0,0 +1,7 @@
+import {PageContainer} from "../../components/pageContainer";
+
+export function HomePage(props){
+    return <PageContainer>
+        Hello
+    </PageContainer>
+}
---
+++ whatever/src/containers/HomePage/topSection.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const TopSectionContainer = styled.div`
+`;
+
+export function TopSection(props) {
+
+}
